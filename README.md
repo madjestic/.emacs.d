0613884f09a7aa31906b7230ca8807064509de27
madjestic <madjestic13@gmail.com>
2019-02-02 11:14:17 +0100

    fixing the lsp-server, adding setup instructions

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # .emacs.d
-my emacs setup
+A Centaur-Emacs on a diet.
+
+Make sure that the following binaries are present on your system:
+
+```
+/usr/local/bin/cquery
+/usr/local/bin/clang
+/usr/local/bin/clang++
+/usr/local/bin/clangd
+```
+(or the equivalents inside `usr/bin/`
+
+- that is needed and used by lsp server
+
+
--- lisp/init-c.el
+++ lisp/init-c.el
@@ -21,42 +21,45 @@
     :diminish
     :init (modern-c++-font-lock-global-mode t))
 
-  (use-package irony
-    :defines (irony-mode-map irony-server-w32-pipe-buffer-size)
-    :hook (((c-mode c++-mode objc-mode) . irony-mode)
-           (irony-mode . irony-cdb-autosetup-compile-options))
-    :config
-    ;; Windows performance tweaks
-    (when (boundp 'w32-pipe-read-delay)
-      (setq w32-pipe-read-delay 0))
-    ;; Set the buffer size to 64K on Windows (from the original 4K)
-    (when (boundp 'w32-pipe-buffer-size)
-      (setq irony-server-w32-pipe-buffer-size (* 64 1024)))
+(use-package cquery
+  :config((setq cquery-executable "/usr/local/bin/cquery")))
 
-    (with-eval-after-load 'counsel
-      (bind-keys :map irony-mode-map
-                 ([remap completion-at-point] . counsel-irony)
-                 ([remap complete-symbol] . counsel-irony)))
+(use-package irony
+  :defines (irony-mode-map irony-server-w32-pipe-buffer-size)
+  :hook (((c-mode c++-mode objc-mode) . irony-mode)
+         (irony-mode . irony-cdb-autosetup-compile-options))
+  :config
+  ;; Windows performance tweaks
+  (when (boundp 'w32-pipe-read-delay)
+    (setq w32-pipe-read-delay 0))
+  ;; Set the buffer size to 64K on Windows (from the original 4K)
+  (when (boundp 'w32-pipe-buffer-size)
+    (setq irony-server-w32-pipe-buffer-size (* 64 1024)))
 
-    (use-package irony-eldoc
-      :hook (irony-mode . irony-eldoc))
+  (with-eval-after-load 'counsel
+    (bind-keys :map irony-mode-map
+               ([remap completion-at-point] . counsel-irony)
+               ([remap complete-symbol] . counsel-irony)))
 
-    (with-eval-after-load 'company
-      (use-package company-irony
-        :defines company-backends
-        :init (cl-pushnew 'company-irony company-backends))
-      (use-package company-irony-c-headers
-        :init (cl-pushnew 'company-irony-c-headers company-backends)))
+  (use-package irony-eldoc
+    :hook (irony-mode . irony-eldoc))
 
-    (with-eval-after-load 'flycheck
-      (use-package flycheck-irony
-        :hook (flycheck-mode . flycheck-irony-setup))))
-
-  ;; Company mode backend for C/C++ header files
   (with-eval-after-load 'company
-    (use-package company-c-headers
+    (use-package company-irony
       :defines company-backends
-      :init (cl-pushnew 'company-c-headers company-backends))))
+      :init (cl-pushnew 'company-irony company-backends))
+    (use-package company-irony-c-headers
+      :init (cl-pushnew 'company-irony-c-headers company-backends)))
+
+  (with-eval-after-load 'flycheck
+    (use-package flycheck-irony
+      :hook (flycheck-mode . flycheck-irony-setup)))))
+
+;; Company mode backend for C/C++ header files
+(with-eval-after-load 'company
+  (use-package company-c-headers
+    :defines company-backends
+    :init (cl-pushnew 'company-c-headers company-backends)))
 
 (provide 'init-c)
 
